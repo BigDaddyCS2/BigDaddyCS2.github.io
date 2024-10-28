26fa9e28d119d9f327022859ba1c5a07d2bae89c
BigDaddyCS2 <jayvanth284@gmail.com>
2024-10-27 20:42:24 -0400

    afg

---
+++ script.js
@@ -0,0 +1,12 @@
+let navbar = `
+<div class="navbar">
+    <a href="index.html">Home</a>
+    <a href="Mirage.html">Mirage</a>
+    <a href="Inferno.html">Inferno</a>
+    <a href="Vertigo.html">Vertigo</a>
+</div>
+`
+
+window.onload = function(e) {
+    document.querySelector(".navbar").outerHTML = navbar;
+}
