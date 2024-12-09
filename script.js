e2755b830b7ac2be76ee6f452f81159b09a9d00e
BigDaddyCS2 <jayvanth284@gmail.com>
2024-12-08 21:38:49 -0500

    anubis

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let navbar = `
 <div class="navbar">
     <a href="index.html">Home</a>
-    <a href="Mirage.html">Mirage</a>
-    <a href="Inferno.html">Inferno</a>
-    <a href="Vertigo.html">Vertigo</a>
     <a href="Ancient.html">Ancient</a>
+    <a href="Anubis.html">Anubis</a>
     <a href="DustII.html">Dust II</a>
+    <a href="Inferno.html">Inferno</a>
+    <a href="Mirage.html">Mirage</a>
     <a href="Nuke.html">Nuke</a>
     <a href="Train.html">Train</a>
+    <a href="Vertigo.html">Vertigo</a>
 </div>
 `
 
